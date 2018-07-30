55aa58023e4dcdfbdaf742fe4f573faff889cbd1
AntonioMarrero <antonio.marrerobonilla@gmail.com>
2018-07-30 15:39:13 -0400

    First Commit

---
+++ src/main/java/com/revature/ERSServerSide/ErsServerSideApplication.java
@@ -0,0 +1,12 @@
+package com.revature.ERSServerSide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsServerSideApplication.class, args);
+	}
+}
